e2000c22508bc2291ed7ae545ead80ae90102f35
Karol Jajko <karoljajko@gmail.com>
2022-12-28 17:29:54 +0100

    add function change properties items hero

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+let descriptionItems
+let popupInput
+let popupAceptBtn
+let popupCancelBtn
+let itemsHeroEdit
+
+const main = () => {
+	prepareDOMElement()
+	prepareDOMEvents()
+}
+
+const prepareDOMElement = () => {
+	descriptionItems = document.querySelector('.description__items')
+	popupInput = document.querySelector('.popup__input')
+	popupAceptBtn = document.querySelector('.actept')
+	popupCancelBtn = document.querySelector('.cancel')
+}
+
+const prepareDOMEvents = () => {
+	descriptionItems.addEventListener('click', checkItems)
+	popupAceptBtn.addEventListener('click', editItems)
+}
+
+const checkItems = (e) => {
+	if(e.target.matches('span')) {
+		itemValue(e)
+	}
+}
+
+const itemValue = (eventFromCheckItems) => {
+	itemsHeroEdit = eventFromCheckItems.target.closest('span')
+	popupInput.value = itemsHeroEdit.firstChild.textContent
+}
+
+const editItems = () => {
+	itemsHeroEdit.firstChild.textContent = popupInput.value
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // automatyczna zmiana daty w footer
 const footerYear = document.querySelector('.footer__year')
 
@@ -7,3 +60,6 @@ const handleCurrentYear = () => {
 }
 // Wywołanie funkcji
 handleCurrentYear()
+
+// nasłuchiwanie na całą stronę, scripty nie uruchomią się zanim niewczyta się cała strona.
+document.addEventListener('DOMContentLoaded', main)
