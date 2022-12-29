8cd6dd95e597bbc3260c20ab5bc99e7103799ab2
Karol Jajko <karoljajko@gmail.com>
2022-12-29 17:12:07 +0100

    edited popup properties items hero

--- main.js
+++ main.js
@@ -1,55 +1,79 @@
+// zmienne globalne do popup
 let descriptionItems
+let popup
 let popupInput
+let popupInfo
 let popupAceptBtn
 let popupCancelBtn
 let itemsHeroEdit
 
+// główna funkcja do wywołania funkcji
 const main = () => {
 	prepareDOMElement()
 	prepareDOMEvents()
 }
 
+// funkcja pobierania elementów
 const prepareDOMElement = () => {
 	descriptionItems = document.querySelector('.description__items')
+	popup = document.querySelector('.popup')
 	popupInput = document.querySelector('.popup__input')
 	popupAceptBtn = document.querySelector('.actept')
 	popupCancelBtn = document.querySelector('.cancel')
+	popupInfo = document.querySelector('.popup__info')
 }
 
+// funkcja nasłuchująca
 const prepareDOMEvents = () => {
 	descriptionItems.addEventListener('click', checkItems)
 	popupAceptBtn.addEventListener('click', editItems)
+	popupCancelBtn.addEventListener('click', cancelPopup)
 }
 
-const checkItems = (e) => {
-	if(e.target.matches('span')) {
+// funkcja sprawdzająca czy właściwy element został kliknięty
+const checkItems = e => {
+	if (e.target.matches('span')) {
 		itemValue(e)
+		popup.style.display = 'block'
 	}
 }
 
-const itemValue = (eventFromCheckItems) => {
+// funkcja wpisująca wybraną wartość do inputa
+const itemValue = eventFromCheckItems => {
+	let characterProperties
+	if (eventFromCheckItems.target.matches('#age')) {
+		characterProperties = 'age'
+	} else if (eventFromCheckItems.target.matches('#growth')) {
+		characterProperties = 'growth'
+	} else if (eventFromCheckItems.target.matches('#weight')) {
+		characterProperties = 'weight'
+	} else if (eventFromCheckItems.target.matches('#eyes')) {
+		characterProperties = 'eyes'
+	} else if (eventFromCheckItems.target.matches('#skin')) {
+		characterProperties = 'skin'
+	} else if (eventFromCheckItems.target.matches('#hair')) {
+		characterProperties = 'hair'
+	}
+
+	popupInfo.textContent = characterProperties
+
 	itemsHeroEdit = eventFromCheckItems.target.closest('span')
 	popupInput.value = itemsHeroEdit.firstChild.textContent
 }
 
+// funkcja wprowadzająca zmiany z inputa do elementu
 const editItems = () => {
 	itemsHeroEdit.firstChild.textContent = popupInput.value
+	popupInfo.textContent = 'Select option'
+	popupInput.value = ''
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// funkcja zamykająca popup
+const cancelPopup = () => {
+	popup.style.display = 'none'
+	popupInfo.textContent = 'Select option'
+	popupInput.value = ''
+}
 
 // automatyczna zmiana daty w footer
 const footerYear = document.querySelector('.footer__year')
